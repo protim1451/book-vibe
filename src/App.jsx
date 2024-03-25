f93b85afeb8472ac4cbc1530ba73da86b1271a27
protim1451 <partha.doict@gmail.com>
2024-03-25 20:47:24 +0600

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-6xl text-red-600 font-extrabold'>Vite + React</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
